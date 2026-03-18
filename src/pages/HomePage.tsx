9c2338453103131be16a6c4d07d206ba71b40004
erioluwa <erioluwaadewoyin@gmail.com>
2026-03-18 12:49:46 +0100

    ...

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import TrustBar from '../components/TrustBar';
+import FeaturedCourses from '../components/FeaturedCourses';
+import WhyChoose from '../components/WhyChoose';
+import Testimonials from '../components/Testimonials';
+import CTABanner from '../components/CTABanner';
+import Footer from '../components/Footer';
+
+export const HomePage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <HeroSection />
+        <TrustBar />
+        <FeaturedCourses />
+        <WhyChoose />
+        <Testimonials />
+        <CTABanner />
+      </main>
+      <Footer />
+    </div>
+  );
+};
