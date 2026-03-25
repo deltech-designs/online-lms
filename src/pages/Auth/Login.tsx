3623573600e3c577505184aba38d2441ea618dc2
deltechdesigns <deltechdesigns@gmail.com>
2026-03-25 15:01:00 +0100

    sign up

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,63 +1,7 @@
 import { useState } from "react"; 
-const BookOpenIcon = () => ( 
-<svg viewBox="0 0 24 24" fill="none" className="w-5 h-5 text-white" stroke="currentColor" 
-strokeWidth={2}> 
-<path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 
-3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 
-2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 
-18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /> 
-</svg> 
-); 
-const EnvelopeIcon = () => ( 
-<svg viewBox="0 0 24 24" fill="none" className="w-4 h-4 text-gray-400" 
-stroke="currentColor" strokeWidth={1.5}> 
-<path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 
-01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 
-2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 
-0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /> 
-</svg> 
-); 
-const LockIcon = () => ( 
-<svg viewBox="0 0 24 24" fill="none" className="w-4 h-4 text-gray-400" 
-stroke="currentColor" strokeWidth={1.5}> 
-<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 
-0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 
-2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" /> 
-</svg> 
-); 
-const MoonIcon = () => ( 
-<svg viewBox="0 0 24 24" fill="none" className="w-5 h-5 text-gray-500" 
-stroke="currentColor" strokeWidth={1.5}> 
-<path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 
-15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 
-16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" /> 
-</svg> 
-); 
-const GoogleIcon = () => ( 
-<svg viewBox="0 0 24 24" className="w-4 h-4" xmlns="http://www.w3.org/2000/svg"> 
-<path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 
-3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/> 
-    <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 
-0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/> 
-    <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 
-10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/> 
-    <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 
-3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/> 
-  </svg> 
-); 
- 
-const CheckBadgeIcon = () => ( 
-  <svg viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-blue-300"> 
-    <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 
-1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 
-4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 
-0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 
-01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 
-4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 
-0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" /> 
-  </svg> 
-); 
- 
+import { BookOpenIcon, EnvelopeIcon, LockIcon, GoogleIcon, CheckBadgeIcon } from "../../utils/Icon";
+import supabase from '../../utils/supabase'; 
+
 export default function LoginPage() { 
   const [showPassword] = useState(false); 
   const [form, setForm] = useState({ email: "", password: "" }); 
@@ -65,6 +9,8 @@ export default function LoginPage() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
     setForm((prev) => ({ ...prev, [e.target.name]: e.target.value })); 
   }; 
+
+  
  
   return ( 
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6 font-sans 
